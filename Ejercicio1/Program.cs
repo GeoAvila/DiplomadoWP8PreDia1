4f80f6bb654c67dfd63d84eb128908590fe5d4a0
GeoAvila <geovanny.avila@gmail.com>
2013-05-30 23:49:55 -0600

    Fin de Ejercicio Pre Dia 1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,17 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            //args = parametros desde consola
+            var nombre = args.SingleOrDefault();
+            if (nombre == null)
+            {
+                Console.WriteLine("Escribe tu nombre y presiona enter al finalizar");
+                //se lee el parametro ingresado
+                nombre = Console.ReadLine();
+            }
+            //imprime Hola y el nombre ingresado
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
         }
     }
 }
